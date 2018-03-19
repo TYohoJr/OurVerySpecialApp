ea09197ecae51156b09b1f940fcd8a4e8083e942
Thomas Yoho <TYohoJr@Gmail.com>
2018-03-19 13:52:14 -0600

    added texts for music venues

--- src/App.js
+++ src/App.js
@@ -17,6 +17,9 @@ export default class App extends Component {
     this.sendDavesText = this.sendDavesText.bind(this);
     this.sendHeebsText = this.sendHeebsText.bind(this);
     this.testSms = this.testSms.bind(this);
+    this.sendFillingText = this.sendFillingText.bind(this);
+    this.sendZebraText = this.sendZebraText.bind(this);
+    this.sendRialtoText = this.sendRialtoText.bind(this);
     this.state = {
       number: "",
       password: "",
@@ -24,26 +27,12 @@ export default class App extends Component {
       tncSubscribe: "",
       heebsSubscribe: "",
       davesSubscribe: "",
+      fillingSubscribe:"",
+      zebraSubscribe:"",
+      rialtoSubscribe:"",
       userProfile: ""
     }
   }
-
-sendFillingAlert(){
-  axios.post("/textFilling", {username:this.state.username, number:this.state.number, token: localStorage.getItem("token")}).then((result)=>{
-    this.setState({
-      fillingSubscribe: result.data.fillingSubscribe,
-      userProfile:
-      <div id="profile-box">
-            <p><div className="profile-base-text">Username: </div>{this.state.username}</p>
-            <p><div className="profile-base-text">Number: </div>{result.data.number}</p>
-            <p><div className="profile-base-text">Your Subscriptions:</div></p><p className="profile-subs">{result.data.tncSubscribe}</p>
-            <p className="profile-subs">{result.data.heebsSubscribe}</p><p className="profile-subs">{result.data.davesSubscribe}</p>
-            <button id="test-sms" className="profile-buttons" onClick={this.testSms}>Test SMS</button>
-            <button id="stop-sms" className="profile-buttons" onClick={this.stopSms}>Stop All SMS</button>
-          </div>
-    })
-  })
-}
 
   sendTnCText() {
     axios.post("/textTnC", { username: this.state.username, number: this.state.number, token: localStorage.getItem("token") }).then((result) => {
@@ -54,8 +43,10 @@ sendFillingAlert(){
           <div id="profile-box">
             <p><div className="profile-base-text">Username: </div>{this.state.username}</p>
             <p><div className="profile-base-text">Number: </div>{result.data.number}</p>
-            <p><div className="profile-base-text">Your Subscriptions:</div></p><p className="profile-subs">{result.data.tncSubscribe}</p><p className="profile-subs">{result.data.heebsSubscribe}</p><p className="profile-subs">{result.data.davesSubscribe}</p>
-            <button id="test-sms" className="profile-buttons" onClick={this.testSms}>Test SMS</button>
+            <p><div className="profile-base-text">Your Subscriptions:</div></p><p className="profile-subs">{result.data.tncSubscribe}</p>
+            <p className="profile-subs">{result.data.heebsSubscribe}</p><p className="profile-subs">{result.data.davesSubscribe}</p>
+            <p className="profile-subs">{result.data.fillingSubscribe}</p><p className="profile-subs">{result.data.zebraSubscribe}</p>
+            <p className="profile-subs">{result.data.rialtoSubscribe}</p>            <button id="test-sms" className="profile-buttons" onClick={this.testSms}>Test SMS</button>
             <button id="stop-sms" className="profile-buttons" onClick={this.stopSms}>Stop All SMS</button>
           </div>
       })
@@ -71,8 +62,10 @@ sendFillingAlert(){
           <div id="profile-box">
             <p><div className="profile-base-text">Username: </div>{this.state.username}</p>
             <p><div className="profile-base-text">Number: </div>{result.data.number}</p>
-            <p><div className="profile-base-text">Your Subscriptions:</div></p><p className="profile-subs">{result.data.tncSubscribe}</p><p className="profile-subs">{result.data.heebsSubscribe}</p><p className="profile-subs">{result.data.davesSubscribe}</p>
-            <button id="test-sms" className="profile-buttons" onClick={this.testSms}>Test SMS</button>
+            <p><div className="profile-base-text">Your Subscriptions:</div></p><p className="profile-subs">{result.data.tncSubscribe}</p>
+            <p className="profile-subs">{result.data.heebsSubscribe}</p><p className="profile-subs">{result.data.davesSubscribe}</p>
+            <p className="profile-subs">{result.data.fillingSubscribe}</p><p className="profile-subs">{result.data.zebraSubscribe}</p>
+            <p className="profile-subs">{result.data.rialtoSubscribe}</p>            <button id="test-sms" className="profile-buttons" onClick={this.testSms}>Test SMS</button>
             <button id="stop-sms" className="profile-buttons" onClick={this.stopSms}>Stop All SMS</button>
           </div>
       })
@@ -88,7 +81,67 @@ sendFillingAlert(){
           <div id="profile-box">
             <p><div className="profile-base-text">Username: </div>{this.state.username}</p>
             <p><div className="profile-base-text">Number: </div>{result.data.number}</p>
-            <p><div className="profile-base-text">Your Subscriptions:</div></p><p className="profile-subs">{result.data.tncSubscribe}</p><p className="profile-subs">{result.data.heebsSubscribe}</p><p className="profile-subs">{result.data.davesSubscribe}</p>
+            <p><div className="profile-base-text">Your Subscriptions:</div></p><p className="profile-subs">{result.data.tncSubscribe}</p>
+            <p className="profile-subs">{result.data.heebsSubscribe}</p><p className="profile-subs">{result.data.davesSubscribe}</p>
+            <p className="profile-subs">{result.data.fillingSubscribe}</p><p className="profile-subs">{result.data.zebraSubscribe}</p>
+            <p className="profile-subs">{result.data.rialtoSubscribe}</p>            <button id="test-sms" className="profile-buttons" onClick={this.testSms}>Test SMS</button>
+            <button id="stop-sms" className="profile-buttons" onClick={this.stopSms}>Stop All SMS</button>
+          </div>
+      })
+    })
+  }
+
+  sendZebraText() {
+    axios.post("/textZebra", { username: this.state.username, number: this.state.number, token: localStorage.getItem("token") }).then((result) => {
+      console.log(result.data)
+      this.setState({
+        zebraSubscribe: result.data.zebraSubscribe,
+        userProfile:
+          <div id="profile-box">
+            <p><div className="profile-base-text">Username: </div>{this.state.username}</p>
+            <p><div className="profile-base-text">Number: </div>{result.data.number}</p>
+            <p><div className="profile-base-text">Your Subscriptions:</div></p><p className="profile-subs">{result.data.tncSubscribe}</p>
+            <p className="profile-subs">{result.data.heebsSubscribe}</p><p className="profile-subs">{result.data.davesSubscribe}</p>
+            <p className="profile-subs">{result.data.fillingSubscribe}</p><p className="profile-subs">{result.data.zebraSubscribe}</p>
+            <p className="profile-subs">{result.data.rialtoSubscribe}</p>            <button id="test-sms" className="profile-buttons" onClick={this.testSms}>Test SMS</button>
+            <button id="stop-sms" className="profile-buttons" onClick={this.stopSms}>Stop All SMS</button>
+          </div>
+      })
+    })
+  }
+
+  sendRialtoText() {
+    axios.post("/textRialto", { username: this.state.username, number: this.state.number, token: localStorage.getItem("token") }).then((result) => {
+      console.log(result.data)
+      this.setState({
+        rialtoSubscribe: result.data.rialtoSubscribe,
+        userProfile:
+          <div id="profile-box">
+            <p><div className="profile-base-text">Username: </div>{this.state.username}</p>
+            <p><div className="profile-base-text">Number: </div>{result.data.number}</p>
+            <p><div className="profile-base-text">Your Subscriptions:</div></p><p className="profile-subs">{result.data.tncSubscribe}</p>
+            <p className="profile-subs">{result.data.heebsSubscribe}</p><p className="profile-subs">{result.data.davesSubscribe}</p>
+            <p className="profile-subs">{result.data.fillingSubscribe}</p><p className="profile-subs">{result.data.zebraSubscribe}</p>
+            <p className="profile-subs">{result.data.rialtoSubscribe}</p>            <button id="test-sms" className="profile-buttons" onClick={this.testSms}>Test SMS</button>
+            <button id="stop-sms" className="profile-buttons" onClick={this.stopSms}>Stop All SMS</button>
+          </div>
+      })
+    })
+  }
+
+
+  sendFillingText() {
+    axios.post("/textFilling", { username: this.state.username, number: this.state.number, token: localStorage.getItem("token") }).then((result) => {
+      this.setState({
+        fillingSubscribe: result.data.fillingSubscribe,
+        userProfile:
+          <div id="profile-box">
+            <p><div className="profile-base-text">Username: </div>{this.state.username}</p>
+            <p><div className="profile-base-text">Number: </div>{result.data.number}</p>
+            <p><div className="profile-base-text">Your Subscriptions:</div></p><p className="profile-subs">{result.data.tncSubscribe}</p>
+            <p className="profile-subs">{result.data.heebsSubscribe}</p><p className="profile-subs">{result.data.davesSubscribe}</p>
+            <p className="profile-subs">{result.data.fillingSubscribe}</p><p className="profile-subs">{result.data.zebraSubscribe}</p>
+            <p className="profile-subs">{result.data.rialtoSubscribe}</p>
             <button id="test-sms" className="profile-buttons" onClick={this.testSms}>Test SMS</button>
             <button id="stop-sms" className="profile-buttons" onClick={this.stopSms}>Stop All SMS</button>
           </div>
@@ -99,30 +152,32 @@ sendFillingAlert(){
   signIn() {
     axios.post("/signInData", { username: this.state.username, password: this.state.password }).then((result) => {
       console.log(result.data.message)
-      if(result.data.message === "Login successful!"){
-      localStorage.setItem('token', result.data.myToken);
-      this.setState({
-        number: result.data.number,
-        modal: !this.state.modal,
-        password: "",
-        tncSubscribe: result.data.tncSubscribe,
-        heebsSubscribe: result.data.heebsSubscribe,
-        davesSubscribe: result.data.davesSubscribe,
-        userProfile:
-          <div id="profile-box">
-            <p><div className="profile-base-text">Username: </div>{this.state.username}</p>
-            <p><div className="profile-base-text">Number: </div>{result.data.number}</p>
-            <p><div className="profile-base-text">Your Subscriptions:</div></p><p className="profile-subs">{result.data.tncSubscribe}</p><p className="profile-subs">{result.data.heebsSubscribe}</p><p className="profile-subs">{result.data.davesSubscribe}</p>
-            <button id="test-sms" className="profile-buttons" onClick={this.testSms}>Test SMS</button>
-            <button id="stop-sms" className="profile-buttons" onClick={this.stopSms}>Stop All SMS</button>
-          </div>
-      })
-    } else {
-      alert(result.data.message)
-    }
+      if (result.data.message === "Login successful!") {
+        localStorage.setItem('token', result.data.myToken);
+        this.setState({
+          number: result.data.number,
+          modal: !this.state.modal,
+          password: "",
+          tncSubscribe: result.data.tncSubscribe,
+          heebsSubscribe: result.data.heebsSubscribe,
+          davesSubscribe: result.data.davesSubscribe,
+          userProfile:
+            <div id="profile-box">
+              <p><div className="profile-base-text">Username: </div>{this.state.username}</p>
+              <p><div className="profile-base-text">Number: </div>{result.data.number}</p>
+              <p><div className="profile-base-text">Your Subscriptions:</div></p><p className="profile-subs">{result.data.tncSubscribe}</p>
+              <p className="profile-subs">{result.data.heebsSubscribe}</p><p className="profile-subs">{result.data.davesSubscribe}</p>
+              <p className="profile-subs">{result.data.fillingSubscribe}</p><p className="profile-subs">{result.data.zebraSubscribe}</p>
+              <p className="profile-subs">{result.data.rialtoSubscribe}</p>              <button id="test-sms" className="profile-buttons" onClick={this.testSms}>Test SMS</button>
+              <button id="stop-sms" className="profile-buttons" onClick={this.stopSms}>Stop All SMS</button>
+            </div>
+        })
+      } else {
+        alert(result.data.message)
+      }
       console.log(this.state.tncSubscribe)
     })
-  
+
   }
 
   testSms() {
@@ -171,7 +226,7 @@ sendFillingAlert(){
     })
   }
 
-  render() { 
+  render() {
     return (
       <div className="App" >
         <Navbar2 signIn={this.signIn} onPasswordChange={this.onPasswordChange}
@@ -181,7 +236,8 @@ sendFillingAlert(){
         {this.state.userWelcome}
         <div id="page-header">
         </div>
-        <Tabs sendTnCText={this.sendTnCText} sendHeebsText={this.sendHeebsText} sendDavesText={this.sendDavesText} />
+        <Tabs sendTnCText={this.sendTnCText} sendHeebsText={this.sendHeebsText} sendDavesText={this.sendDavesText} 
+        sendFillingText={this.sendFillingText} sendZebraText={this.sendZebraText} sendRialtoText={this.sendRialtoText}/>
       </div>
     );
   }
